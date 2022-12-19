3ce5a281ed4588eaedef4048d7e9d06321b48aa9
aymenbouhaha <92325496+aymenbouhaha@users.noreply.github.com>
2022-12-19 16:08:52 +0100

    first_commit

---
+++ lib/models/product_model.dart
@@ -0,0 +1,10 @@
+class Product {
+  int id;
+  String nom;
+  String description;
+  double price;
+  String imageSrc;
+
+  Product(this.id, this.nom, this.description, this.price, this.imageSrc);
+
+}
