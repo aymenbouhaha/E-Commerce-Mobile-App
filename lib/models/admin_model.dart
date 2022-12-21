2b4b5ec2bc85aae9556d8f1807d0631ee43e88d3
aymenbouhaha <92325496+aymenbouhaha@users.noreply.github.com>
2022-12-21 03:04:50 +0100

    config2

--- lib/models/admin_model.dart
+++ lib/models/admin_model.dart
@@ -5,4 +5,5 @@ class Admin extends User{
 
   Admin(int id, String username, String email, String phoneNumber, String password, String imageSrc) : super(id, username, email, phoneNumber, password, imageSrc);
 
+
 }
