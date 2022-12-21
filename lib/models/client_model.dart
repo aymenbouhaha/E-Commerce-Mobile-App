6c8d3536ff041bd6b045bcba915ead467b0e7708
aymenbouhaha <92325496+aymenbouhaha@users.noreply.github.com>
2022-12-21 03:17:21 +0100

    config2

--- lib/models/client_model.dart
+++ lib/models/client_model.dart
@@ -5,4 +5,5 @@ class Client extends User{
 
   Client(int id, String username, String email, String phoneNumber, String password, String imageSrc) : super(id, username, email, phoneNumber, password, imageSrc);
 
+
 }
